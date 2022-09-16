00d378a353cd2688614c4e06b6b2379a1d809571
PavlovAles <97961953+PavlovAles@users.noreply.github.com>
2022-09-16 10:33:14 +0300

    feat: crash server on /crash-test route

--- backend/app.js
+++ backend/app.js
@@ -25,6 +25,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
 app.use(errorLogger);
 app.use(errors());
